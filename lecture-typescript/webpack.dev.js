32135ed1bfb47ce2b54925aa8ad23578b898a246
takayuki.kayawari <kayamelo@gmail.com>
2021-12-24 16:08:42 +0900

    Move to new directory

---
+++ lecture-typescript/webpack.dev.js
@@ -0,0 +1,21 @@
+const path = require('path');
+module.exports = {
+  entry: './food-app/main.ts',
+  mode: 'development',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '/dist/'
+  },
+  devtool: 'inline-source-map',
+  module: {
+    rules: [{
+      test: /\.ts$/,
+      use: 'ts-loader',
+      exclude: /node_modules/
+    }]
+  },
+  resolve: {
+    extensions: ['.ts', '.js']
+  }
+}
